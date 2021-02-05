9ad77bce971f2821e7100558f4c264cb89cff06c
Brian Giannini <brian.giannini.1@gmail.com>
2021-02-05 14:51:03 +0100

    add repository and viewmodel

--- app/src/main/java/io/sangui/androidreferenceproject/MovieRepository.kt
+++ app/src/main/java/io/sangui/androidreferenceproject/MovieRepository.kt
@@ -3,4 +3,19 @@ package io.sangui.androidreferenceproject
 import android.app.Application
 
 class MovieRepository(application: Application) {
+
+    private var database = MovieDatabase.getInstance(application)
+    private var movieDao: MovieDao
+
+    init {
+        movieDao = database.movieDao()
+    }
+
+    fun insert(movie: Movie) {
+        movieDao.insert(movie)
+    }
+
+    fun delete(movie: Movie) {
+        movieDao.delete(movie)
+    }
 }
--- app/src/main/java/io/sangui/androidreferenceproject/MovieViewModel.kt
+++ app/src/main/java/io/sangui/androidreferenceproject/MovieViewModel.kt
@@ -1,4 +1,16 @@
 package io.sangui.androidreferenceproject
 
-class MovieViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MovieViewModel(app: Application): AndroidViewModel(app) {
+    private val repository = MovieRepository(app)
+
+    fun insert(movie: Movie) {
+        repository.insert(movie)
+    }
+
+    fun delete(movie: Movie) {
+        repository.delete(movie)
+    }
 }
