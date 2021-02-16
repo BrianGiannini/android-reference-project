e8c00867ed19dfa7f4d78de41f42f99ef1c53e29
Brian Giannini <brian.giannini.1@gmail.com>
2021-02-16 01:21:41 +0100

    work on nav graph transition

--- app/src/main/java/io/sangui/androidreferenceproject/ui/AddEditMovieFragment.kt
+++ app/src/main/java/io/sangui/androidreferenceproject/ui/AddEditMovieFragment.kt
@@ -1,33 +1,22 @@
 package io.sangui.androidreferenceproject.ui
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import io.sangui.androidreferenceproject.R
 import io.sangui.androidreferenceproject.databinding.AddEditMovieFragmentBinding
 
-const val EXTRA_ID = "EXTRA_ID"
-const val EXTRA_TITLE = "EXTRA_TITLE"
-const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
-const val EXTRA_NOTE = "EXTRA_NOTE"
 
 class AddEditMovieFragment : Fragment(R.layout.add_edit_movie_fragment) {
 
-    private lateinit var mode: Mode
-    private var movieId: Int = -1
+    private val args: AddEditMovieFragment by navArgs()
 
     private lateinit var binding: AddEditMovieFragmentBinding
-
-    companion object {
-        fun newInstance() = AddEditMovieFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,55 +33,37 @@ class AddEditMovieFragment : Fragment(R.layout.add_edit_movie_fragment) {
                 saveMovie()
             }
         }
-
         return view
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        movieId = intent.getIntExtra(EXTRA_ID, -1)
-//        mode = if (movieId == -1) Mode.EditMovie
-//        else Mode.EditMovie
-
-//        when (mode) {
-//            Mode.AddMovie -> title = "Add Movie"
-//            Mode.EditMovie -> {
-//                title = "Edit Movie"
-//                with(binding) {
-//                    etTitle.setText(intent.getStringExtra(EXTRA_TITLE))
-//                    etDesc.setText(intent.getStringExtra(EXTRA_DESCRIPTION))
-//                    numberPickerPriority.value = intent.getIntExtra(EXTRA_NOTE, -1)
-//                }
-//            }
-//        }
     }
 
     private fun saveMovie() {
         val title = binding.etTitle.text.toString()
         val desc = binding.etDesc.text.toString()
-        val priority = binding.numberPickerPriority.value
+        val note = binding.numberPickerPriority.value
 
-//        if (title.isEmpty()) {
-//            Toast.makeText(this, "please insert title and description", Toast.LENGTH_SHORT).show()
-//            return
-//        }
-//
-//        val data = Intent()
-//
-//        if (movieId != -1)
-//            data.putExtra(EXTRA_ID, movieId)
-//        data.putExtra(EXTRA_TITLE, title)
-//        data.putExtra(EXTRA_DESCRIPTION, desc)
-//        data.putExtra(EXTRA_NOTE, priority)
-//
-//        setResult(Activity.RESULT_OK, data)
-//        finish()
+        if (title.isEmpty()) {
+            Toast.makeText(context, "please insert title and description", Toast.LENGTH_SHORT).show()
+        }
+
+        val action = AddEditMovieFragmentDirections.actionAddEditMovieFragmentToMoviesListFragment(title, desc, note)
+        findNavController().navigate(action)
+
     }
 
-    private sealed class Mode {
-        object AddMovie : Mode()
-        object EditMovie : Mode()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        when (args.mode) {
+//            Mode.AddMovie -> title = "Add Movie"
+//            Mode.EditMovie -> {
+//                title = "Edit Movie"
+//                with(binding) {
+////                    etTitle.setText(args.title)
+////                    etDesc.setText(args.description)
+////                    numberPickerPriority.value = args.value
+//                }
+//            }
+//        }
     }
 
 }
--- app/src/main/java/io/sangui/androidreferenceproject/ui/MoviesListFragment.kt
+++ app/src/main/java/io/sangui/androidreferenceproject/ui/MoviesListFragment.kt
@@ -1,16 +1,14 @@
 package io.sangui.androidreferenceproject.ui
 
-import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,18 +17,12 @@ import io.sangui.androidreferenceproject.MovieAdapter
 import io.sangui.androidreferenceproject.R
 import io.sangui.androidreferenceproject.databinding.MoviesListFragmentBinding
 
-const val ADD_MOVIE_REQUEST = 1
-const val EDIT_MOVIE_REQUEST = 2
-
 class MoviesListFragment : Fragment(R.layout.movies_list_fragment) {
 
+    private val args: MoviesListFragmentArgs by navArgs()
     private lateinit var vm: MovieViewModel
     private lateinit var binding: MoviesListFragmentBinding
     private lateinit var adapter: MovieAdapter
-
-    companion object {
-        fun newInstance() = MoviesListFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,45 +37,35 @@ class MoviesListFragment : Fragment(R.layout.movies_list_fragment) {
 
         vm = ViewModelProvider(this).get(MovieViewModel::class.java)
         vm.getAllMovies().observe(viewLifecycleOwner, {
-//            adapter.submitList(it)
+            adapter.submitList(it)
         })
+
+
+        vm.insert(Movie(args.title, args.description, args.note))
+        Toast.makeText(context, "Movie inserted!", Toast.LENGTH_SHORT).show()
+        vm.update(Movie(args.title, args.description, args.note))
+        Toast.makeText(context, "Movie updated!", Toast.LENGTH_SHORT).show()
+
+
+
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonAddMovie.setOnClickListener {
-            val action = MoviesListFragmentDirections.actionMoviesListFragmentToAddEditMovieFragment()
+    private fun setUpRecyclerView() {
+        adapter = MovieAdapter { clickedMovie ->
+            val description = clickedMovie.description ?: ""
+            val action = MoviesListFragmentDirections.actionMoviesListFragmentToAddEditMovieFragment(clickedMovie.title, description, clickedMovie.note)
             findNavController().navigate(action)
         }
-    }
 
-    private fun setUpRecyclerView() {
-//        adapter = MovieAdapter { clickedMovie ->
-//            val intent = Intent(this, AddEditMovieActivity::class.java)
-//            intent.putExtra(EXTRA_ID, clickedMovie.id)
-//            intent.putExtra(EXTRA_TITLE, clickedMovie.title)
-//            intent.putExtra(EXTRA_DESCRIPTION, clickedMovie.description)
-//            intent.putExtra(EXTRA_NOTE, clickedMovie.note)
-//            startActivityForResult(intent, EDIT_MOVIE_REQUEST)
-//        }
-//
-//        with(binding) {
-//            recyclerView.layoutManager = LinearLayoutManager(context)
-//            recyclerView.setHasFixedSize(true)
-//            recyclerView.adapter = adapter
-//        }
+        with(binding) {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
+        }
     }
 
     private fun setUpListeners() {
-        binding.buttonAddMovie.setOnClickListener {
-            val flowStepNumberArg = 1
-//            val action = MoviesListFragmentDirection.nextAction(flowStepNumberArg)
-//            findNavController().navigate(action)
-//            val intent = Intent(this, AddEditMovieActivity::class.java)
-//            startActivityForResult(intent, ADD_MOVIE_REQUEST)
-        }
 
         // swipe listener
         ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
